245d3a45d96ab7cc74b7346ade0acc4bd4881c1e
GVERES74 <djveresg@freemail.hu>
2020-07-14 21:42:10 +0200

    Keyevent added

--- src/pkg_Arvenar_Main/ArvenarGameGUI.java
+++ src/pkg_Arvenar_Main/ArvenarGameGUI.java
@@ -4,36 +4,42 @@
  * and open the template in the editor.
  */
 package pkg_Arvenar_Main;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javax.swing.JTextField;
+import pkg_Characters.Summon_Characters;
 
 /**
  *
  * @author TE332168
  */
-public class ArvenarGameGUI {
+public class ArvenarGameGUI{
     
     Stage gamestage = new Stage();
     Pane gamepane = new Pane();
-    Pane gameareapane = new Pane();
+    Pane gameareapane = new Pane(); 
     Scene gamescene = new Scene(gamepane);
     MPlayer play_music;
     ImageView cmpass = new ImageView();
+    Label dialog_text = new Label();
+    Summon_Characters enemy = new Summon_Characters();
         
     int hero_x = 300, hero_y = 150;
     int pirate_x = 205, pirate_y = 175;
     
-    final int max_x = 400, max_y = 400;
-    final int min_x = 30, min_y = 30;
+    final int MAX_X = 400, MAX_Y = 400;
+    final int MIN_X = 30, MIN_Y = 30;
     
     Button btn_playgame = new Button("Play game");
     Button btn_exitgame = new Button("Stop game");
@@ -43,6 +49,7 @@ public class ArvenarGameGUI {
     Button right = new Button("E");
     Button hero = new Button("H");
     Button pirate = new Button("P");
+        
     
     public ArvenarGameGUI() throws FileNotFoundException{ //ez nem lehet void, különben üres stage-t kapsz vissza!!
       
@@ -50,6 +57,7 @@ public class ArvenarGameGUI {
         
         gamestage.setTitle("Arvenar main run");
         gamestage.setScene(gamescene);
+        
         
          btn_playgame.setLayoutX(50); btn_playgame.setLayoutY(400);
          btn_exitgame.setLayoutX(50); btn_exitgame.setLayoutY(450);
@@ -71,17 +79,27 @@ public class ArvenarGameGUI {
         cmpass.setLayoutX(90); cmpass.setLayoutY(210);
         gamestage.initModality(Modality.APPLICATION_MODAL);
         gameareapane.getChildren().addAll(hero, pirate);
+        dialog_text.setLayoutX(70); dialog_text.setLayoutY(500);
         
-        gamepane.getChildren().addAll(cmpass, btn_playgame, btn_exitgame, gameareapane, up, down, left, right);
+        gamepane.getChildren().addAll(cmpass, btn_playgame, btn_exitgame, gameareapane, up, down, left, right, dialog_text);
         
-         
-                      
+        
+        gamescene.setOnKeyPressed(event ->{
+            switch  (event.getCode()){
+                case UP: if (hero_y!= MIN_Y){hero.setLayoutY(hero_y -=5);} else hero_y = MIN_Y + 5; check_HeroPos();break;
+                case DOWN: if (hero_y!= MAX_Y){hero.setLayoutY(hero_y +=5);} else hero_y = MAX_Y - 5; check_HeroPos();break;
+                case LEFT: if (hero_x!= MIN_X){hero.setLayoutX(hero_x -=5);} else hero_x = MIN_X + 5; check_HeroPos();break;
+                case RIGHT: if (hero_x!= MAX_X){hero.setLayoutX(hero_x +=5);} else hero_x = MAX_X - 5; check_HeroPos();break;
+            }
+        });
+        
+        
         up.setOnAction(action -> {
             
-            if (hero_y!= min_y){ 
+            if (hero_y!= MIN_Y){ 
                 hero.setLayoutY(hero_y -=5);
             }
-            else hero_y = min_y + 5;
+            else hero_y = MIN_Y + 5;
             
             check_HeroPos();
                 
@@ -89,11 +107,11 @@ public class ArvenarGameGUI {
         
         down.setOnAction(action -> {
             
-            if (hero_y!= max_y){
+            if (hero_y!= MAX_Y){
                 
                 hero.setLayoutY(hero_y +=5);
             }
-            else hero_y = max_y - 5;
+            else hero_y = MAX_Y - 5;
             
             check_HeroPos();
             
@@ -102,10 +120,10 @@ public class ArvenarGameGUI {
         
         left.setOnAction(action -> {
             
-            if (hero_x!= min_x){ 
+            if (hero_x!= MIN_X){ 
                 hero.setLayoutX(hero_x -=5);
             }
-            else hero_x = min_x + 5;
+            else hero_x = MIN_X + 5;
             
             check_HeroPos();
             
@@ -114,10 +132,10 @@ public class ArvenarGameGUI {
         
         right.setOnAction(action -> {
             
-            if (hero_x!= max_x){ 
+            if (hero_x!= MAX_X){ 
                 hero.setLayoutX(hero_x +=5);
             }
-            else hero_x = max_x - 5;
+            else hero_x = MAX_X - 5;
             
             
             check_HeroPos();
@@ -125,25 +143,36 @@ public class ArvenarGameGUI {
         });
         
        //----------------------------------------------- 
-        btn_exitgame.setOnAction(action -> {
+        
+             
+       btn_playgame.setOnAction(action -> {
+            int rx, ry;
+            
+            rx = (int)(Math.random()*(MAX_X-MIN_X)+1);
+            ry = (int)(Math.random()*(MAX_Y-MIN_Y)+1);
+            
+            pirate.setLayoutX(rx); pirate.setLayoutY(ry);
+        });
+              
+       
+       btn_exitgame.setOnAction(action -> {
             
             gamestage.close();
         });
-        
-        
-        
     
     }
         
+        
     public void check_HeroPos(){
             
-           if (((hero.getLayoutY() < pirate.getLayoutY()+30) && (hero.getLayoutX() < pirate.getLayoutX()+30)) 
-                   && ((hero.getLayoutY() > pirate.getLayoutY()-30) && (hero.getLayoutX() > pirate.getLayoutX()-30))) {
+           if (((hero.getLayoutY() < pirate.getLayoutY()+20) && (hero.getLayoutX() < pirate.getLayoutX()+20)) 
+                   && ((hero.getLayoutY() > pirate.getLayoutY()-20) && (hero.getLayoutX() > pirate.getLayoutX()-20))) {
                 
                 hero.setText("Fucked");
-            
-        }
-            else hero.setText("H");
+                dialog_text.setText("You were fucked up by "+enemy.croco1.getFname());
+           }
+           
+           else hero.setText("H");
     }        
     
     public void show_GameGUI(){
@@ -151,6 +180,6 @@ public class ArvenarGameGUI {
          gamestage.show();
     }
     
-    
+
     }
 
