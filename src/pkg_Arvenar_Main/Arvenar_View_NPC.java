5e6ed228bc68dc0b269919aac5f31527818b16db
GVERES74 <djveresg@freemail.hu>
2020-07-31 23:02:19 +0200

    Level maps created

--- src/pkg_Arvenar_Main/Arvenar_View_NPC.java
+++ src/pkg_Arvenar_Main/Arvenar_View_NPC.java
@@ -22,6 +22,7 @@ import pkg_Characters.Character_DataBase_NPC;
 import pkg_Characters.Character_DataBase_PC;
 import pkg_Characters.Summon_Characters;
 
+
 /**
  *
  * @author TE332168
@@ -31,14 +32,14 @@ public class Arvenar_View_NPC {
      int i = 0;
     
     Summon_Characters clone = new Summon_Characters();
-    Character_DataBase_NPC npcdbase = new Character_DataBase_NPC();
-    MPlayer play_music; //ne példányosítsd!!
-        
+    Character_DataBase_NPC cdbase_npc = new Character_DataBase_NPC();
+            
     FileInputStream inputimg = new FileInputStream("src/img/npc_animal_bbear1.jpg");
     Image selectedpcimage = new Image(inputimg);
     ImageView npc_imgview = new ImageView(selectedpcimage);
     TextField npc_name_textfield = new TextField(); 
     TextArea npc_bio_textarea = new TextArea();
+    TextArea npc_description_textarea = new TextArea();
     TextArea npc_stats_textarea = new TextArea();
     
     Stage stagename = new Stage();
@@ -48,28 +49,23 @@ public class Arvenar_View_NPC {
                     
     public Arvenar_View_NPC() throws FileNotFoundException {
         
-        npcdbase.dataBase(); //Karakter adatbázis inicializálása
+        //cdbase_npc.Character_DataBase_NPC(); //Karakter adatbázis inicializálása. Nem kell ha konstruktort hozol létre: "public Character_DataBase_NPC()" és ebben töltöd fel az ArrayList-et. Nem lehet void!!
         
         npc_name_textfield.setLayoutX(305); npc_name_textfield.setLayoutY(50);
-        npc_name_textfield.setText(clone.bbear1.getFname());
+       
         npc_name_textfield.setEditable(false); npc_name_textfield.setFocusTraversable(false);
             
         npc_bio_textarea.setLayoutX(100); npc_bio_textarea.setLayoutY(100); npc_bio_textarea.setWrapText(true); npc_bio_textarea.setMaxHeight(100);
-        npc_bio_textarea.setText(clone.bbear1.getBiography());
+       
         npc_bio_textarea.setEditable(false); npc_bio_textarea.setFocusTraversable(false);
         
         npc_stats_textarea.setLayoutX(100); npc_stats_textarea.setLayoutY(220); npc_stats_textarea.setWrapText(true); npc_stats_textarea.setMaxWidth(200.0);
         npc_stats_textarea.setEditable(false); npc_stats_textarea.setFocusTraversable(false);
-        npc_stats_textarea.appendText("Race: "+npcdbase.npc_stats_race.get(i)); 
-        npc_stats_textarea.appendText("\nCast: "+npcdbase.npc_stats_cast.get(i)); 
-        npc_stats_textarea.appendText("\nHealth: "+npcdbase.npc_stats_maxhealth.get(i));
-        npc_stats_textarea.appendText("\nWeapon: "+npcdbase.npc_stats_weapon.get(i)); 
-        npc_stats_textarea.appendText("\nBattle shout: "+npcdbase.npc_stats_shout.get(i)); 
         
-                    
-        npc_imgview.setLayoutX(600);
-        npc_imgview.setLayoutY(100);
-        
+        npc_description_textarea.setLayoutX(310); npc_description_textarea.setLayoutY(220); npc_description_textarea.setWrapText(true); npc_description_textarea.setMaxWidth(250.0);
+        npc_description_textarea.setEditable(false); npc_description_textarea.setFocusTraversable(false);
+                            
+        getNonPlayableCharacter();
         
         stagename.setTitle("View non-playable characters");
         stagename.setResizable(false);
@@ -83,7 +79,7 @@ public class Arvenar_View_NPC {
         Button nextbutton = new Button("Next"); nextbutton.setLayoutX(500); nextbutton.setLayoutY(50);
         Button prevbutton = new Button("Previous"); prevbutton.setLayoutX(200); prevbutton.setLayoutY(50);
         
-        panename.getChildren().addAll(choosebutton, cancelbutton, nextbutton, prevbutton, this.npc_name_textfield, this.npc_bio_textarea, npc_imgview, npc_stats_textarea);
+        panename.getChildren().addAll(choosebutton, cancelbutton, nextbutton, prevbutton, this.npc_name_textfield, this.npc_bio_textarea, npc_stats_textarea, npc_description_textarea);
                 
         useArrowKeys();
     //ActionListener block ----------------------------------------------------------------------------------
@@ -97,62 +93,29 @@ public class Arvenar_View_NPC {
         });
         
         //Back to previous page (Main menu)------------------------------------------------------------------
-        cancelbutton.setOnAction(Action -> {
-           
-            stagename.close();
+        cancelbutton.setOnAction(Action -> stagename.close());
                                   
-        });
-        
-        
+               
         //Get the next Player---------------------------------------------------------------------------------
-        nextbutton.setOnAction(Action -> {
-         
-            nextNpc();
+        nextbutton.setOnAction(Action -> nextNpc());
                 
-        }); 
         //--------------------------------------------------------------------------------------------------------
         
         //Get the previous Player---------------------------------------------------------------------------------
-        prevbutton.setOnAction(Action -> {
-            
-            previousNpc();
-                    
-        });
+        prevbutton.setOnAction(Action -> previousNpc());
                 
     }
         
         public void nextNpc(){
         
-           if(i < npcdbase.npc_img.size()-1){
+            if(i < cdbase_npc.npc_character.size()-1){
                     i++;
-                    }
+            }
                     else {
-                        i = npcdbase.npc_img.size()-1;
+                        i = cdbase_npc.npc_character.size()-1;
                     }
-                try {
                     npc_imgview.setImage(null); //ImageView "törlése", különben egymásra pakolja az image-ket.
-                    
-                    inputimg = new FileInputStream(npcdbase.npc_img.get(i));
-                    npc_name_textfield.setText(npcdbase.npc_name.get(i));
-                    npc_bio_textarea.setText(npcdbase.npc_bio.get(i));
-                    npc_stats_textarea.clear();
-                    npc_stats_textarea.appendText("Race: "+npcdbase.npc_stats_race.get(i)); 
-                    npc_stats_textarea.appendText("\nCast: "+npcdbase.npc_stats_cast.get(i)); 
-                    npc_stats_textarea.appendText("\nHealth: "+npcdbase.npc_stats_maxhealth.get(i));
-                    npc_stats_textarea.appendText("\nWeapon: "+npcdbase.npc_stats_weapon.get(i)); 
-                    npc_stats_textarea.appendText("\nBattle shout: "+npcdbase.npc_stats_shout.get(i)); 
-                    this.npc_name = npcdbase.npc_name.get(i);
-                    
-                                        
-                    npc_imgview = new ImageView(new Image(inputimg));
-                    npc_imgview.setLayoutX(600);
-                    npc_imgview.setLayoutY(100);
-                    panename.getChildren().add(npc_imgview);
-                    
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(ArvenarSetPC.class.getName()).log(Level.SEVERE, null, ex);
-                
-            } 
+                    getNonPlayableCharacter();
         }
         
         public void previousNpc(){
@@ -165,41 +128,54 @@ public class Arvenar_View_NPC {
                     else {
                         i = 0;
                     }            
-                    try {
-                    inputimg = new FileInputStream(npcdbase.npc_img.get(i));
-                    npc_name_textfield.setText(npcdbase.npc_name.get(i));
-                    npc_bio_textarea.setText(npcdbase.npc_bio.get(i));
-                    npc_stats_textarea.clear();
-                    npc_stats_textarea.appendText("Race: "+npcdbase.npc_stats_race.get(i)); 
-                    npc_stats_textarea.appendText("\nCast: "+npcdbase.npc_stats_cast.get(i)); 
-                    npc_stats_textarea.appendText("\nHealth: "+npcdbase.npc_stats_maxhealth.get(i));
-                    npc_stats_textarea.appendText("\nWeapon: "+npcdbase.npc_stats_weapon.get(i)); 
-                    npc_stats_textarea.appendText("\nBattle shout: "+npcdbase.npc_stats_shout.get(i)); 
-                    this.npc_name = npcdbase.npc_name.get(i);
-                        
-                                                
-                    } catch (FileNotFoundException ex) {
-                        Logger.getLogger(ArvenarSetPC.class.getName()).log(Level.SEVERE, null, ex);
-                    };
-                
-            npc_imgview = new ImageView(new Image(inputimg));
-            npc_imgview.setLayoutX(600);
-            npc_imgview.setLayoutY(100);
-            panename.getChildren().add(npc_imgview); 
+                    getNonPlayableCharacter();
         }
       
         public String getNpcName(){
-            return npc_name;
+            return cdbase_npc.getNPC(i).getFname();
         }
         
         public void useArrowKeys(){
-        scenename.setOnKeyPressed(event ->{
-            switch  (event.getCode()){
-                case RIGHT: nextNpc();break;
-                case LEFT: previousNpc(); break;
+            scenename.setOnKeyPressed(event ->{
+                switch  (event.getCode()){
+                 case RIGHT: nextNpc();break;
+                    case LEFT: previousNpc(); break;
             }
         
-        });
+            });
     
         }
-}
+        
+        public void getNonPlayableCharacter(){
+            
+         try {
+             inputimg = new FileInputStream(cdbase_npc.npc_img.get(i));
+             npc_name_textfield.setText(cdbase_npc.getNPC(i).getFname());
+             npc_bio_textarea.setText(cdbase_npc.getNPC(i).getBiography());
+             npc_stats_textarea.clear();
+             npc_description_textarea.clear();
+             npc_description_textarea.appendText(cdbase_npc.getNPC(i).getNpcDesc());
+             
+             
+             npc_stats_textarea.appendText("Race: "+cdbase_npc.getNPC(i).getRace());
+             npc_stats_textarea.appendText("\nCast: "+cdbase_npc.getNPC(i).getCast());
+             npc_stats_textarea.appendText("\nHealth: "+cdbase_npc.getNPC(i).getHealth_point());
+             npc_stats_textarea.appendText("\nWeapon: "+cdbase_npc.getNPC(i).getFav_weapon());
+             npc_stats_textarea.appendText("\nBattle shout: "+cdbase_npc.getNPC(i).getShout());
+             this.npc_name = cdbase_npc.getNPC(i).getFname();
+             
+             
+             
+             npc_imgview = new ImageView(new Image(inputimg));
+             npc_imgview.setLayoutX(600);
+             npc_imgview.setLayoutY(100);
+             panename.getChildren().add(npc_imgview);
+         } catch (FileNotFoundException ex) {
+             Logger.getLogger(Arvenar_View_NPC.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+        }
+        
+        
+        }
+
